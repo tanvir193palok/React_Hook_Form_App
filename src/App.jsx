f2542933534ec7250b1f8968ca7643485eccef9f
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-02-22 19:54:04 +0600

    Added registration form and validation functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import LoginForm from "./forms/LoginForm";
+// import LoginForm from "./forms/LoginForm";
+import RegistrationForm from "./forms/RegistrationForm";
 
 const App = () => {
   return (
     <>
-      <LoginForm />
+      {/*<LoginForm />*/}
+      <RegistrationForm />
     </>
   );
 };
--- src/forms/RegistrationForm.jsx
+++ src/forms/RegistrationForm.jsx
@@ -3,7 +3,7 @@ import Field from "../components/Field";
 import FieldSet from "../components/FieldSet";
 import { useForm } from "react-hook-form";
 
-const LoginForm = () => {
+const RegistrationForm = () => {
   const {
     register,
     handleSubmit,
@@ -12,23 +12,13 @@ const LoginForm = () => {
   } = useForm();
 
   const submitForm = (formData) => {
-    const user = { email: "x@example.com", password: "12345678" };
-
-    const found =
-      formData.email === user.email && formData.password === user.password;
-
-    if (!found) {
-      setError("root.random", {
-        message: `User with email "${formData.email}" is not found`,
-        type: "random",
-      });
-    }
+    console.log(formData);
   };
 
   return (
     <div className="flex flex-col justify-center items-center">
       <form onSubmit={handleSubmit(submitForm)}>
-        <FieldSet label="Enter Login Details">
+        <FieldSet label="Enter Your Details">
           <Field label="Email" error={errors.email}>
             <input
               {...register("email", { required: "Email is required." })}
@@ -59,11 +49,43 @@ const LoginForm = () => {
               }`}
             />
           </Field>
+          <Field label="Full Name" error={errors.fname}>
+            <input
+              {...register("fname", {
+                required: "Full Name is required",
+              })}
+              type="text"
+              name="fname"
+              id="fname"
+              placeholder="Enter full name"
+              className={`p-2 border box-border w-[300px] rounded-md ${
+                errors.fname ? "border-red-500" : "border-gray-200"
+              }`}
+            />
+          </Field>
+          <Field label="Age" error={errors.age}>
+            <input
+              {...register("age", {
+                required: "Age is required",
+                max: {
+                  value: 100,
+                  message: "Age must be between 0 and 100",
+                },
+              })}
+              type="number"
+              name="age"
+              id="age"
+              placeholder="Enter age"
+              className={`p-2 border box-border w-[300px] rounded-md ${
+                errors.age ? "border-red-500" : "border-gray-200"
+              }`}
+            />
+          </Field>
         </FieldSet>
         <div>{errors?.root?.random?.message}</div>
         <Field>
           <button className="text-md text-white m-auto cursor-pointer p-1 border rounded-lg bg-purple-500">
-            Login
+            Register
           </button>
         </Field>
       </form>
@@ -71,4 +93,4 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default RegistrationForm;
